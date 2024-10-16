fad07ad51c812a26a1bfb084bef055abeb819dd7
matheus <matheuslima199913@hotmail.com>
2024-10-16 11:41:21 -0300

    scanner_cisco

--- scanner_cisco.py
+++ scanner_cisco.py
@@ -19,3 +19,15 @@ def scan_network_for_cisco(network):
             print(f"Não foi possível identificar o sistema operacional para o host {host}")
 
     return cisco_ips
+
+# Exemplo de uso
+if __name__ == "__main__":
+    network_to_scan = '10.52.100.0/24'  # Rede a ser escaneada
+    cisco_ips = scan_network_for_cisco(network_to_scan)
+    
+    if cisco_ips:
+        print("Dispositivos Cisco encontrados:")
+        for ip in cisco_ips:
+            print(ip)
+    else:
+        print("Nenhum dispositivo Cisco encontrado na rede.")
